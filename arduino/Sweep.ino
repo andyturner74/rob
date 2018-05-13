df73d99d67ed94d51b7c10954e6821ff7cdbc4c4
Andy Turner <andyturner74@yahoo.com>
2018-05-13 17:16:38 -0500

    put ultrasonic sensor on a servo

---
+++ arduino/Sweep.ino
@@ -0,0 +1,49 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+int ledPin = 13;
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+char receivedChar;
+
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  receivedChar = '0';
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read();
+
+    if (receivedChar == '1') {
+      for (pos = 90; pos <= 180; pos += 2) { // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        Serial.println(pos);
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      myservo.write(90);
+      for (pos = 90; pos >= 0; pos -= 2) { // goes from 180 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        Serial.println(pos);
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+      myservo.write(90);
+    }
+  }
+}
+
--- rover_avoiding.py
+++ rover_avoiding.py
@@ -4,14 +4,73 @@
 
 from rrb3 import *
 import time, random
+import serial
 
 BATTERY_VOLTS = 9
 MOTOR_VOLTS = 6
 
 rr = RRB3(BATTERY_VOLTS, MOTOR_VOLTS)
 
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
 # if you dont have a switch, change the value below to True
 running = False
+
+distance_left = []
+distance_right = []
+
+def backup():
+    rr.stop()
+    rr.reverse(0.5, 0.33)
+
+
+def clear_right():
+    ser.write('1') # tell arduino to start sweeping sensor left and right
+    prev_distance = 5000
+    prev_angle = 90
+
+    ser.flushInput()
+
+    while True:
+        distance = rr.get_distance()
+        angle = ser.readline()
+
+        if int(angle) > prev_angle: # scanning left
+            distance_left.append(distance)
+        else: # scanning right
+            distance_right.append(distance)
+        prev_angle = int(angle)
+        time.sleep(0.05)
+        if int(angle) < 10: # done scanning
+            break
+
+    # get average of distance_right and distance_left
+    if len(distance_right) > 0:
+        average_right = sum(distance_right) / float(len(distance_right))
+    else:
+        average_right = 0
+        
+           
+    if len(distance_left) > 0:
+        average_left = sum(distance_left) / float(len(distance_left))
+    else:
+        average_left = 0
+
+    print 'average left is ' + str(average_left) + '\n'
+    print 'average right is ' + str(average_right) + '\n'
+
+    if average_right > average_left:
+        return True
+    else:
+        return False
+
+def turn_right():
+    rr.right(0.25, 0.25)
+
+def turn_left():
+    rr.left(0.25, 0.25)
+
+
 
 def turn_randomly():
     turn_time = random.randint(1, 3)
@@ -24,9 +83,17 @@ def turn_randomly():
 try:
     while True:
         distance = rr.get_distance()
-        print(distance)
+#        print(distance)
         if distance < 50 and running:
-            turn_randomly()
+            backup()
+            if clear_right():
+                turn_right()
+            else:
+                turn_left()
+
+        del distance_right[:]
+        del distance_left[:]
+
         if running:
             rr.forward(0, 0.33)  # set forward speed to 30% of full speed
         if rr.sw2_closed():
